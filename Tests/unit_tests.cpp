1694e952890cf7832eb922ea556076447d83843d
Piyush Karki <piyush.karki25@gmail.com>
2023-01-30 19:35:48 +0100

    Test added for MPI RMA

--- Tests/unit_tests.cpp
+++ Tests/unit_tests.cpp
@@ -10,6 +10,136 @@
 #include "SWE-Solvers/Source/FWaveVecSolver.hpp"
 #include "Tools/RealType.hpp"
 
+#include <mpi.h>
+#include <vector>
+
+void exchangeLayers_h(
+  const int    leftNeighborRank,
+  double*      o_leftInflow,
+  double*      leftOutflow,
+  const int    rightNeighborRank,
+  double*      o_rightInflow,
+  double*      rightOutflow,
+  MPI_Datatype mpiCol,
+  int          mpicol_len
+) {
+  // Create the window objects
+  MPI_Win leftWin, rightWin;
+
+  // if (rightNeighborRank >= 0) { // Create the windows for the left and right ghost layers
+  MPI_Win_create(
+    leftOutflow, (mpicol_len) * sizeof(double), sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &rightWin
+  );
+
+  MPI_Win_fence(0, rightWin);
+  MPI_Get(o_rightInflow, 1, mpiCol, rightNeighborRank, 0, 1, mpiCol, rightWin);
+  MPI_Win_fence(0, rightWin);
+  
+
+  MPI_Win_free(&rightWin);
+
+  // if (leftNeighborRank >= 0) {
+  MPI_Win_create(
+    rightOutflow, (mpicol_len) * sizeof(double), sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &leftWin
+  );
+
+  MPI_Win_fence(0, leftWin);
+  MPI_Get(o_leftInflow, 1, mpiCol, leftNeighborRank, 0, 1, mpiCol, leftWin);
+  MPI_Win_fence(0, leftWin);
+  MPI_Win_free(&leftWin);
+}
+
+
+TEST_CASE("exchangeLayers_h", "[ExchangeLayers]") {
+
+  int world_rank, worldsize;
+  int* argc;
+  char*** argv;
+  int mpicol_len = 10;
+  MPI_Init(argc,argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &worldsize);
+  
+  // Set up the MPI datatype
+  MPI_Datatype mpiCol;
+  MPI_Type_vector(1, mpicol_len, 1, MPI_DOUBLE, &mpiCol);
+  MPI_Type_commit(&mpiCol);
+  double* leftOutflow;
+  double* rightOutflow;
+  double* leftInflow;
+  double* rightInflow;
+
+ 
+
+  double a[mpicol_len];
+  double b[mpicol_len];
+  double c[mpicol_len];
+  double d[mpicol_len];
+
+  double w[mpicol_len];
+  double x[mpicol_len];
+  double y[mpicol_len];
+  double z[mpicol_len];
+
+  for (int i =0 ; i<mpicol_len; ++i)
+  {   
+  a[i] =  i*(world_rank)+i;                                                                     
+  b[i] = i*(world_rank) + 1;
+  c[i] =  3*(world_rank);                                                                              
+  d[i] =  (world_rank) - 1;
+  }
+  if (world_rank==1)
+  {
+  for (int i =0 ; i<mpicol_len; ++i)
+  {                                                                            
+    w[i] =   i*(world_rank-1)+i;                                                                          
+    x[i] = i*(world_rank-1) + 1;                                                                                             
+    y[i] =     3*(world_rank-1);                                                                              
+    z[i] =     (world_rank-1)-1;                                                                            
+  }
+  }
+  if (world_rank==0)
+  {
+    for (int i =0 ; i<mpicol_len; ++i)
+    {
+      w[i] = i*(world_rank+1)+i;
+      x[i] = i*(world_rank+1) + 1;
+      y[i] = 3*(world_rank+1);
+      z[i] = (world_rank+1)-1;
+    }
+  }
+
+  leftOutflow = &a[0];
+  rightOutflow = &b[0];
+  leftInflow = &c[0];
+  rightInflow = &d[0];
+
+  
+
+  int leftNeighborRank = (world_rank == 0) ? MPI_PROC_NULL : world_rank - 1;
+  int rightNeighborRank = (world_rank == worldsize-1) ? MPI_PROC_NULL : world_rank + 1;
+
+  exchangeLayers_h(
+    leftNeighborRank, leftInflow, leftOutflow,
+    rightNeighborRank, rightInflow, rightOutflow,
+    mpiCol, mpicol_len
+  );
+  
+  if (leftNeighborRank != MPI_PROC_NULL) {
+    for (int i = 0; i < mpicol_len; i++) {
+      REQUIRE(*(leftInflow+i) == Approx(x[i]));
+    }
+  }
+
+  if (rightNeighborRank != MPI_PROC_NULL) {
+    for (int i = 0; i < mpicol_len; i++) {
+      // std::cout<<rightInflow
+      REQUIRE(*(rightInflow+i) == Approx(w[i]));
+    }
+  }
+  MPI_Finalize();
+}
+
 TEST_CASE("computeNetUpdates is Tested for vectorization", "[computeNetUpdates]") {
   Solvers::FWaveVecSolver<RealType>        fWaveVecSolver;
   std::default_random_engine               generator;
